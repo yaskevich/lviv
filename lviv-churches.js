306dd80d8983d0ceca59adf7cf1b8662383715b2
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-05-16 20:13:08 +0300

    July 2017

---
+++ lviv-churches.js
@@ -0,0 +1,317 @@
+var geojson = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2017-07-26T13:44:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1935011263",
+        "amenity": "place_of_worship",
+        "denomination": "ukrainian_greek_catholic",
+        "name": "Церква Миколая Чернецького",
+        "religion": "christian"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0150782,
+          49.868064
+        ]
+      },
+      "id": "node/1935011263"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2489222533",
+        "amenity": "place_of_worship",
+        "name": "Церква"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0966776,
+          49.8429597
+        ]
+      },
+      "id": "node/2489222533"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2498922292",
+        "amenity": "place_of_worship",
+        "building": "yes",
+        "denomination": "orthodox",
+        "religion": "christian"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.9645957,
+          49.7784649
+        ]
+      },
+      "id": "node/2498922292"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3734462654",
+        "amenity": "place_of_worship",
+        "denomination": "greek_catholic",
+        "name": "Церква святих Ольги і Єлизавети",
+        "name:de": "Elisabethenkirche",
+        "name:en": "Church of St. Olha and Elizabeth",
+        "name:pl": "Kościół św. Elżbiety",
+        "name:ru": "Церковь святых Ольги и Елизаветы",
+        "religion": "christian",
+        "wikipedia": "pl:Kościół św. Elżbiety we Lwowie"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0045575,
+          49.8368588
+        ]
+      },
+      "id": "node/3734462654"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3893344050",
+        "amenity": "place_of_worship",
+        "religion": "christian"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.9449614,
+          49.8718656
+        ]
+      },
+      "id": "node/3893344050"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4040139066",
+        "amenity": "place_of_worship",
+        "religion": "christian"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0168644,
+          49.8065622
+        ]
+      },
+      "id": "node/4040139066"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4161650690",
+        "addr:housenumber": "14а",
+        "addr:street": "Бориса Грінченка вулиця",
+        "amenity": "place_of_worship",
+        "name": "Духовна Семінарія Непорочного Серця Матері Божої Фатімської"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0525057,
+          49.8744326
+        ]
+      },
+      "id": "node/4161650690"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4266885047",
+        "amenity": "place_of_worship"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0393253,
+          49.8513516
+        ]
+      },
+      "id": "node/4266885047"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4347863594",
+        "addr:street": "Авраама Лінкольна вулиця",
+        "amenity": "place_of_worship",
+        "denomination": "ukrainian_greek_catholic",
+        "name": "Церква Св. Луїджі Оріоне",
+        "name:ru": "Церковь Св. Луиджи Орионе",
+        "religion": "christian"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.036658,
+          49.8660337
+        ]
+      },
+      "id": "node/4347863594"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4385996716",
+        "amenity": "place_of_worship",
+        "denomination": "catholic",
+        "religion": "christian"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0230229,
+          49.817596
+        ]
+      },
+      "id": "node/4385996716"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4406466896",
+        "amenity": "place_of_worship",
+        "email": "churchrizpb@gmail.com",
+        "name": "храм Різдва Пресвятої Богородиці",
+        "name:uk": "храм Різдва Пресвятої Богородиці",
+        "opening_hours": "Tu, Th-Su 08:00-19:00"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.953607,
+          49.9020081
+        ]
+      },
+      "id": "node/4406466896"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4565815193",
+        "amenity": "place_of_worship"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.9509412,
+          49.8048248
+        ]
+      },
+      "id": "node/4565815193"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4568955006",
+        "amenity": "place_of_worship"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0563874,
+          49.84966
+        ]
+      },
+      "id": "node/4568955006"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4712124684",
+        "addr:housenumber": "2а",
+        "addr:street": "Миколи Скрипника вулиця",
+        "amenity": "place_of_worship",
+        "building": "yes",
+        "denomination": "pentecostal",
+        "name": "Дім молитви Третьої львівської церкви ХВЄ «Голгофа»",
+        "name:en": "Pentecostal Church \"Golgotha\"",
+        "name:ru": "Дом молитвы Третьей львовской церкви ХВЕ «Голгофа»",
+        "religion": "christian"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0530116,
+          49.7896015
+        ]
+      },
+      "id": "node/4712124684"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4742535821",
+        "addr:postcode": "79038",
+        "amenity": "place_of_worship",
+        "name:en": "Kingdom Hall of Jehovah's Witnesses",
+        "name:ru": "Зал Царства Свидетелей Иеговы",
+        "name:uk": "Зал Царства Свідків Єгови"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0749816,
+          49.8212802
+        ]
+      },
+      "id": "node/4742535821"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4806662522",
+        "addr:housenumber": "8",
+        "addr:postcode": "79037",
+        "addr:street": "Кущова вулиця",
+        "amenity": "place_of_worship",
+        "name": "Зал Царства Свідків Єгови",
+        "name:en": "Kingdom Hall of Jehovah's Witnesses",
+        "name:ru": "Зал Царства Свидетелей Иеговы",
+        "name:uk": "Зал Царства Свідків Єгови"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.04394,
+          49.868713
+        ]
+      },
+      "id": "node/4806662522"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4938225787",
+        "addr:housenumber": "4",
+        "addr:street": "Олександра Довженка вулиця",
+        "amenity": "place_of_worship",
+        "name": "Релігійне об'єднання \"Церква Адвентистів Сьомого Дня\"",
+        "religion": "christian"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.0543724,
+          49.7972647
+        ]
+      },
+      "id": "node/4938225787"
+    }
+  ]
+};
---
+++ lviv.js
@@ -0,0 +1,188 @@
+var isRotate = false;
+var timer;
+var defLayerIndex = 10;
+var osm_attr = 'Map data  &copy; <a href="http://www.lvivcenter.org/en/" target="_blank">Center for Urban History of East Central Europe</a> &copy; <a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia maps</a> &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a>';
+// var wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 0.5} );
+var wikimedia = L.tileLayer('https://tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=a0b0612456064c01a5aa81dbb0670862', { maxZoom: 19, minZoom: 6, opacity: 0.5} );
+var sources = [
+	[1, 16, 11, "1. Lemberg, 1848", "Einzahl der Einwohner v. Lemberg, 1848", "04-Zajachkivska 1848.jpg (1)"],
+	[2, 15, 11, "2. Lemberg Leopol Lwow", "", "// 01-BIXa390_col.XXI_sect.262_c.JPEG (2)"],
+	[3, 16, 11, "3. Horbaya plan 1931 r.", "Horbaya plan orientacyjny Wielkiego Lwowa", "02-Horbaya plan orjentacyjny wielkiego Lwowa_1931_id15.jpg (3)"],
+	[4, 17, 11, "4. Plan Król. Stol. Miasta Lwowa", "", "03-nb-lnu_464 II.jpg (4)"],
+	[5, 17, 11, "5. Plan of the City of Lemberg, 1828", "", "id11_1828.jpg (5)"],
+	[6, 17, 11, "6. Plan von Lemberg", "", "19.jpg (6)"],
+	[7, 15, 11, "7. Lwów - Leopol - Lemberg - Львовъ, 1914", "", "1914_Lemberg_Tourist guide_HRC.jpg (7)"],
+	[8, 16, 11, "8. Lviv - Lwow - Leopol, 1915 r.", "", "1915_PL_ka_GIh378-4 1.Expl.jpg (8)"],
+	[9, 15, 11, "9. Plan miasta Lwowa 1916 r.", "", "1916_PL_nl k I 113.797_bw.jpg (9)"],
+	[10, 15, 11, "10. Mapa warstwicowa Lwowa 1917 r.", "", "1917_MWL_HRC.jpg (10)"],
+	// cust
+	[11, 14, 11, "11. Plan Lwowa 1918 r.", "Plan Lwowa według stanu z listopada i grudnia 1918 r.", "1918_PL_Kharchuk.jpg (11)"],
+	[16, 16, 11, "12. Royal and Capital City of Lwow, 1936", "", "2.41.7.jpg (16)"]
+];
+var baseLayers = {};
+for (var i = 0; i < sources.length; ++i){
+	var c = sources[i];
+	baseLayers[c[3]] =  L.tileLayer('https://maps.yaskevich.com/lviv/'+c[0]+'/{z}/{x}/{y}.png', { maxZoom: c[1], minZoom: c[2], attribution: osm_attr});
+}
+var baseLayers2 = {			
+	"Map of the age of Lviv buildings (by Intetics)": L.tileLayer('https://opengeo.intetics.com.ua/buildingage/data/tiles/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, attribution: osm_attr}),
+	"ESRI Satellite": L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'	, { attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, 	IGP, UPR-EGP, and the GIS User Community'}),
+	"Google Satellite" : L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+		maxZoom: 20,
+		subdomains:['mt0','mt1','mt2','mt3'],
+		attribution: "Google Maps"
+	})	
+};
+var street = new L.LayerGroup();
+var churches = new L.LayerGroup();
+var neighborhood = new L.LayerGroup();
+var overlays = {
+	"Staroyevreiska street": street,
+	"Lviv churches (OSM POI)": churches,
+	"Staroyevreiska neighborhood (by <a href='https://drive.google.com/open?id=1iC3yziuM_hg50_cqHPxzvvk5Kc4&usp=sharing_eil' target='_blank'>Eugene Polyakov)": neighborhood
+};
+var myIcon = L.icon({
+	iconUrl: 'synagogue11.svg',
+	// iconSize: [75, 75],
+	// iconAnchor: [37.5, 75]
+	iconSize: [46, 46],
+	iconAnchor: [23, 46]
+});
+var marker = new L.marker([49.84135908817427, 24.034717082977295], {icon: myIcon}).addTo(street);
+var pointA = new L.LatLng(49.84016207033833,24.030178785324097);
+var pointB = new L.LatLng(49.84124838256608,24.03509259223938);
+new L.Polyline([pointA, pointB], {
+	color: 'blue', weight: 5, opacity: 1, smoothFactor: 1
+}).addTo(street);
+var map = L.map('map', { zoomControl:false, center: [49.83, 24.014167], zoom: 14, layers: [wikimedia, baseLayers[sources[defLayerIndex][3]], street]});
+// http://overpass-turbo.eu/s/qCd
+L.control.layers(Object.assign(baseLayers, baseLayers2), overlays).addTo(map);
+
+
+var customControl =  L.Control.extend({
+  options: {
+    position: 'topleft'
+  },
+  onAdd: function (map) {
+    var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom info');
+	container.innerHTML = map.getZoom();
+    // container.onclick = function(){
+      // console.log('buttonClicked');
+    // }
+    return container;
+  },
+  update: function (props) {	
+    this._container.innerHTML = props;
+	}
+});
+
+var info = new customControl();
+map.addControl(info);
+info.update(sources[defLayerIndex][3]);
+
+map.on('baselayerchange', function (e) {
+    info.update(e.name);
+});
+
+
+function onEachFeature(feature, layer) {
+var popupContent = 
+(feature.properties.name?'<b>' + feature.properties.name + '</b></br>':'')+
+(feature.properties.denomination?feature.properties.denomination:'church');
+
+if (feature.properties && feature.properties.popupContent) {
+	popupContent += feature.properties.popupContent;
+}
+
+layer.bindPopup(popupContent);
+}
+
+function onEachFeature2(feature, layer) {
+	var popupContent = 
+	(feature.properties.Name?'<b>' + feature.properties.Name + '</b></br>':'')+
+	(feature.properties.description?feature.properties.description:'');
+	if (feature.properties && feature.properties.popupContent) {
+		popupContent += feature.properties.popupContent;
+	}
+	layer.bindPopup(popupContent);
+}
+
+L.geoJSON([geojson], {
+	style: function (feature) {
+		return feature.properties && feature.properties.style;
+	},
+	onEachFeature: onEachFeature,
+	pointToLayer: function (feature, latlng) {
+		return L.circleMarker(latlng, {
+			radius: 8,
+			fillColor: "white",
+			color: "#000",
+			weight: 1,
+			opacity: 1,
+			fillOpacity: 0.8
+		});
+	}
+}).addTo(churches);
+
+L.geoJSON([jewish], {
+	style: function (feature) {
+		return feature.properties && feature.properties.style;
+	},
+	onEachFeature: onEachFeature2,
+	pointToLayer: function (feature, latlng) {
+		return L.circleMarker(latlng, {
+			radius: 8,
+			fillColor: "yellow",
+			color: "#000",
+			weight: 1,
+			opacity: 1,
+			fillOpacity: 0.8
+		});
+	}
+}).addTo(neighborhood);
+
+// console.log(document.getElementsByClassName('leaflet-control-layers-base')[0]);
+// console.log(document.getElementsByClassName('leaflet-control-layers-base')[0]);
+var layerControlElement = document.getElementsByClassName('leaflet-control-layers')[0];
+function caroussel(cl){
+	timer = setTimeout(function(){ 
+		// bool ^= true;
+		if (isRotate === true){
+			layerControlElement.getElementsByTagName('input')[cl].click();
+			map.invalidateSize();
+			caroussel(cl>13 ? 0: ++cl);
+		}
+	}, 10000);	
+}
+
+
+L.easyButton({
+    states: [{
+            stateName: 'mapswitching-run',        // name the state
+            icon:      'fa-sync-alt',               // and define its properties
+            title:     'Switch map every 10 seconds',      // like its title
+            onClick: function(btn, map) {       // and its callback
+				isRotate = true;
+                caroussel(0);
+                btn.state('mapswitching-stop');    // change state on click!
+            }
+        }, {
+            stateName: 'mapswitching-stop',
+            icon:      'fa-stop-circle',
+            title:     'Stop map rotation',
+            onClick: function(btn, map) {
+                isRotate = false;
+				clearTimeout(timer);
+                btn.state('mapswitching-run');
+            }
+    }]
+}).addTo(map);
+
+
+
+var Zzz = new customControl();
+map.addControl(Zzz);
+map.on('zoomend', function (e) {
+	// console.log(e);
+    Zzz.update(map.getZoom());
+});
